e7909eda0703a8a6492ce165c0871d71116ac338
Tom Fielder <tom.fielder97@gmail.com>
2022-07-26 14:55:40 +0100

    Style basic login form section

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -6,8 +6,8 @@ const Login: FC = (): ReactElement => {
   }, []);
 
   return (
-    <section>
-      <a href="https://localhost:3000/api/user/auth/facebook" className="text-2xl font-bold underline">
+    <section className="bg-white items-center border-none rounded-lg mt-10 mx-0 mb-0 pt-5 px-0 pb-7 w-80 shadow-sm text-center">
+      <a href="https://localhost:3000/api/user/auth/facebook" className="text-2xl font-bold underline text-blue-600">
         Login with Facebook
       </a>
     </section>
