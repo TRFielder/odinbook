9865429d71749154b66d23b3c68e0796999bf6b7
Tom Fielder <tom.fielder97@gmail.com>
2022-08-05 20:29:57 +0100

    Login process now works correctly

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,23 +1,14 @@
-import React, { FC, ReactElement, useEffect } from 'react';
+import React, { FC, ReactElement, useEffect, useContext } from 'react';
+import { UserContext } from '../../utilities/UserContext';
 
 const Login: FC = (): ReactElement => {
-  useEffect(() => {
-    console.log('Loaded Login component');
-    authenticate();
-  }, []);
-
-  const authenticate = async () => {
-    const response = await fetch('https://localhost:3000/api/user/current', { credentials: 'include', mode: 'cors' });
-    const result = await response.json();
-    console.log(result.firstname + ' ' + result.surname);
-  };
+  const { user, setUser } = useContext(UserContext);
 
   return (
     <section className="bg-white items-center border-none rounded-lg mt-10 mx-0 mb-0 pt-5 px-0 pb-7 w-80 shadow-sm text-center">
       <a href="https://localhost:3000/api/user/auth/facebook" className="text-2xl font-bold underline text-blue-600">
         Login with Facebook
       </a>
-      <button onClick={() => authenticate()}>Check login status</button>
     </section>
   );
 };
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,13 +2,36 @@ import React, { FC, ReactElement, useContext, useEffect } from 'react';
 import { UserContext } from '../../utilities/UserContext';
 
 const Profile: FC = (): ReactElement => {
+  const { user, setUser } = useContext(UserContext);
+
   useEffect(() => {
-    console.log('Loaded profile component');
+    console.log('Loaded Login component');
+    authenticate();
   }, []);
+
+  const reportUser = () => {
+    console.log(`My name is ${user?.firstname} ${user?.surname}`);
+  };
+
+  const authenticate = async () => {
+    const response = await fetch('https://localhost:3000/api/user/current', { credentials: 'include', mode: 'cors' });
+    const result = await response.json();
+    console.log(result);
+    setUser({
+      id: result._id,
+      username: result.username,
+      firstname: result.firstname,
+      surname: result.surname,
+      avatar_URL: result.avatar_URL,
+    });
+  };
 
   return (
     <div>
       <h2>Profile component</h2>
+      {user !== null ? <p>Hello, {user.firstname}</p> : <p>Hello</p>}
+      <img src={user?.avatar_URL}></img>
+      <button onClick={() => reportUser()}>Who am i</button>
     </div>
   );
 };
